535f625871c9efdea5b0f3e2e7a3ed1ebe865172
Romna <r.maiorkhak@gmail.com>
2024-07-30 13:11:11 +0000

    initial commit

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -11,11 +11,12 @@ export class UploadService {
   private readonly region: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.region = this.configService.get<string>('AWS_S3_REGION') || 'eu-north-1';
-    this.bucketName = this.configService.get<string>('BUCKET_NAME') || 'myfirstmybuckettest';
-    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY') || 'AKIA4MTWHFEJ3OFFMIEP';
-    const secretAccessKey = this.configService.get<string>('AWS_SECRET_ACCESS_KEY') || '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';
+    this.region = 'eu-north-1'
+    this.bucketName = 'myfirstmybuckettest'
+    const accessKeyId = 'AKIA4MTWHFEJ3OFFMIEP'
+    const secretAccessKey = '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'
 
+    
     this.s3Client = new S3Client({
       region: this.region,
       credentials: {
